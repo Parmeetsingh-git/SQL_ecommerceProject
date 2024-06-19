abe1368ae0ccd78b2fd1945a4854e85e4b55a616
Parmeetsingh-git <parmeetdhade@gmail.com>
2024-06-19 01:47:41 -0400

    trying to upload eda-exploatory data sql file

---
+++ .vscode/EDA_SQL_Olist/view_data.sql
@@ -0,0 +1,37 @@
+-- take a look att the data stored in each table
+--1
+select * from olist_customers
+limit 10;
+
+--2
+select * from olist_geolocation
+limit 10;
+
+--3
+select * from olist_order_items
+limit 10;
+
+--4
+select * from olist_order_payments
+limit 10;
+
+--5
+
+select * from olist_order_reviews
+limit 10;
+
+--6 
+select * from olist_orders
+limit 10;
+
+--7
+select * from olist_product_name_translation
+limit 10;
+
+--8
+select * from olist_products
+limit 10;
+
+--9
+select * from olist_seller
+limit 10;
